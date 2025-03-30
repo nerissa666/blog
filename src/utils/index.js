9f8e0dd31a3c56c82b68e771a79d395c7ef7e717
xgx <xgx@xgxdeMacBook-Pro.local>
2025-03-30 09:04:39 +0800

    env

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,6 +4,7 @@ const formatDate = (date) => {
 };
 const formatSRC = (src) => {
     console.log(process.env.NODE_ENV, 'process.env.NODE_ENV');
+    console.log( process.env.VUE_APP_API_BASE_URL + src, ' process.env.VUE_APP_API_BASE_URL + src');
     return process.env.NODE_ENV === 'production' ?  process.env.VUE_APP_API_BASE_URL + src : src;
 };
 export { formatDate, formatSRC };
